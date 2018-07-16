ef9e6931a648d12c346ce919940ff169db199422
Orlando Jimenez <till.haxor@gmail.com>
2018-07-16 16:23:04 -0300

    update entrypoint

--- java-nr-vault/entrypoint-vault.sh
+++ java-nr-vault/entrypoint-vault.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-if [ ! -z "${VAULT_URL}" ] && [ ! -z "${VAULT_TOKEN}" ]; then
-    curl -k -s -H "X-Vault-Token:${VAULT_TOKEN}" \
-        "${VAULT_URL}/${SERVICE_NAME}/${ENVIRONMENT}/envfile" \
-        | jq -r ".data" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > envfile
-    echo "${VAULT_URL}/${SERVICE_NAME}/${ENVIRONMENT}/envfile"
-    echo `cat envfile`
-    export `cat envfile`
+set -e
+
+if [ ! -z "${VAULT_URL}" ] && [ ! -z "${VAULT_TOKEN}" ]
+then
+    export $(curl -k -s -H "X-Vault-Token:${VAULT_TOKEN}" "${VAULT_URL}" \
+        | jq -r ".data" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]")
 fi
 
 exec "$@"
