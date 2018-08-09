f43e22b7b82487cf7118cf5b441515e875208d1e
Nicolás Allegrotti <ingnico@gmail.com>
2018-08-09 15:35:20 -0300

    One line oneliner

--- java-nr-vault/entrypoint-vault.sh
+++ java-nr-vault/entrypoint-vault.sh
@@ -4,8 +4,7 @@ set -e
 
 if [ ! -z "${VAULT_URL}" ] && [ ! -z "${VAULT_TOKEN}" ]
 then
-    set -a && source <(curl -k -s -H "X-Vault-Token:${VAULT_TOKEN}" "${VAULT_URL}" \
-        | jq -r '.data|to_entries|map("\(.key)=\(.value)")[]') && set +a
+    set -a && source <(curl -k -s -H "X-Vault-Token:${VAULT_TOKEN}" "${VAULT_URL}" | jq -r '.data|to_entries|map("\(.key)=\(.value)")[]') && set +a
 fi
 
 exec "$@"
